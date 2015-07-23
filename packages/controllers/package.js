632885dd0d8aed2d12b74069b72b3cccf150aee0
jgoley <jgoley@gmail.com>
2015-07-23 15:47:57 -0400

    Creates list of coding keywords

--- packages/controllers/package.js
+++ packages/controllers/package.js
@@ -27,5 +27,6 @@ Package.onUse(function(api) {
   api.addFiles('group_detail.coffee', ['client', 'server']);
   api.addFiles('paragraph_text.coffee', 'client');
   api.addFiles('user_form.coffee', 'client');
+  api.addFiles('coding_keywords.coffee', 'client');
 });
 
