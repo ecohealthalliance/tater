ea7265c45f46bcf84acd7b0f80fee078430960c1
Nathan Breit <nath@nathanathan.com>
2015-12-09 10:01:05 -0500

    Adding script for adding headerIds to 'Other' keywords

---
+++ .scripts/database/addHeaderIds.js
@@ -0,0 +1,15 @@
+// Add header ids to code keywords that have a sub-header id but no header id.
+// Usage Example:
+// mongo localhost:3001/meteor addHeaderIds.js
+var keywordsUpdated = 0;
+db.keywords.find({
+    headerId: { $eq: null },
+    subHeaderId: { $ne: null }
+}).forEach(function(keyword){
+    var headerId = db.subHeaders.findOne({_id: keyword.subHeaderId}).headerId;
+    db.keywords.update({_id: keyword._id}, {
+        $set: { headerId: headerId }
+    });
+    keywordsUpdated++;
+});
+print("Updated " + keywordsUpdated + " keywords");
