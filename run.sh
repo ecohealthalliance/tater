c2c762dc186f19380eccce7daf18cd4cdfd7ad8d
Freddie Rosario <frosario@users.noreply.github.com>
2016-02-02 11:09:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ cd /tater/programs/server && npm install
 #service supervisor start
 
 #Run our meteor app in production mode
-export export METEOR_SETTINGS=$(cat /shared/settings-production.json)
+export METEOR_SETTINGS=$(cat /shared/settings-production.json)
 cd /tater && node main.js
