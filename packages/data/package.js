6d2c7204206b5a4abae97235adc26b24e250bc00
Nathan Breit <nath@nathanathan.com>
2016-02-16 14:02:39 -0500

    Add BSVE init file

--- packages/data/package.js
+++ packages/data/package.js
@@ -9,4 +9,5 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use(['coffeescript']);
   api.addFiles('default_codes.coffee', ['server', 'client']);
+  api.addFiles('bsve_init.coffee', ['server']);
 });
