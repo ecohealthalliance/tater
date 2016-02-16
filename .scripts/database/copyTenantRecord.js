09954e40acbb49c072e2a32cc68148a7c836b7d2
Abe Miessler <miessler@ecohealthalliance.org>
2016-02-16 07:35:07 -0800

    setting current tenant in script

--- .scripts/database/copyTenantRecord.js
+++ .scripts/database/copyTenantRecord.js
@@ -2,5 +2,7 @@ var currentTenant = db;
 db = db.getSiblingDB("www");
 var tenant = db.tenants.findOne({'tenantName': currentTenant});
 
+tenant.current = true;
+
 db = db.getSiblingDB(currentTenant);
 db.tenants.insert(tenant);
