c05e815d21afd7f187c94a1e8e402d4f6263ec9a
Abe Miessler <miessler@ecohealthalliance.org>
2017-02-23 14:12:10 -0800

    bsve dossier handler

--- packages/data/package.js
+++ packages/data/package.js
@@ -7,6 +7,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('coffeescript');
+  api.addFiles('postMessageHandler.coffee', 'client')
   api.addFiles([
     'default_codes.coffee',
     'database_seeds.coffee',
